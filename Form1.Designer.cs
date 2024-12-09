9516f81fce27a78cc57b4dd6746956e285555346
lyy <381769019@qq.com>
2024-12-09 13:12:21 +0800

    1-添加图片功能

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,12 +30,12 @@ namespace ListViewTest
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(215, 177);
+            this.button1.Location = new System.Drawing.Point(132, 147);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -43,23 +43,26 @@ namespace ListViewTest
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // textBox1
+            // label1
             // 
-            this.textBox1.Location = new System.Drawing.Point(215, 114);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 21);
-            this.textBox1.TabIndex = 1;
-            this.textBox1.Text = "是否打开新页面？";
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Control;
+            this.label1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(97, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(168, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "请点击【确定】按钮。";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(580, 283);
-            this.Controls.Add(this.textBox1);
+            this.ClientSize = new System.Drawing.Size(354, 229);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Demo";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -68,7 +71,7 @@ namespace ListViewTest
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -19,7 +19,32 @@ namespace ListViewTest
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("确定打开新页面吗？","提示",MessageBoxButtons.YesNoCancel);
+            // 显示消息框，询问用户是否打开新页面
+            DialogResult result = MessageBox.Show("确定打开图片吗？","提示",MessageBoxButtons.YesNoCancel);
+
+            if (result==DialogResult.Yes) 
+            {
+                openImage();
+            }
+
+        }
+        private void openImage() 
+        {
+            Form imageForm = new Form();
+            PictureBox pictureBox = new PictureBox();
+
+            // 设置 PictureBox 属性
+            pictureBox.Dock = DockStyle.Fill;
+            pictureBox.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox.Image = Image.FromFile(@"E:\BEACI照片\IMG_20230906_095119-1.jpg");
+
+            // 将 PictureBox 添加到窗体
+            imageForm.Controls.Add(pictureBox);
+            imageForm.Text = "图片";
+            imageForm.WindowState = FormWindowState.Maximized;
+
+            // 显示窗体
+            imageForm.ShowDialog();
         }
     }
 }
