6ba178f1a2441b6aa84bc0b3b03e238246460d86
lyy <381769019@qq.com>
2024-12-09 10:36:49 +0800

    添加项目文件。

---
+++ Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ListViewTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("确定打开新页面吗？","提示",MessageBoxButtons.YesNoCancel);
+        }
+    }
+}
